21f46e173df6bd1adf963c542ceb7683d9151ebc
otherbanana <34509569+otherbanana@users.noreply.github.com>
2019-11-28 18:30:28 +0800

    Update quanx.js

--- Files/quanx.js
+++ Files/quanx.js
@@ -2,3 +2,5 @@ var body = $response.body;
 body = '\/*\n@supported E4CF989D7F77\n*\/\n' + body;
 
 $done(body);
+//远程引用大佬Js订阅自动加ID
+//^https:\/\/(raw.githubusercontent|\w+\.github)\.(com|io)\/.*\.js$ url script-response-body quanx.js
