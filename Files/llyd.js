e47460b80dc3b5aa182d4bb4026ae125b6de176d
otherbanana <34509569+otherbanana@users.noreply.github.com>
2019-12-01 00:19:00 +0800

    Update llyd.js

--- Files/llyd.js
+++ Files/llyd.js
@@ -3,7 +3,7 @@
 
 因缓存机制，使用脚本后可能需要重装一次「流利说•阅读」后方可生效；生效标志是点进详解不再有限制
 Surge:
-http-response ^https?:\/\/vira\.llsapp\.com\/api\/v2\/readings\/(accessible|limitation) requires-body=1,max-size=0,script-path=scripts/llyd.js
+http-response ^https?:\/\/vira\.llsapp\.com\/api\/v2\/readings\/(accessible|limitation) requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/otherbanana/Script/master/Files/llyd.js
 
 Quantumult X:
 ^https?:\/\/vira\.llsapp\.com\/api\/v2\/readings\/(accessible|limitation) rurl script-response-body Myself/llyd.js
