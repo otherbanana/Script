56bb7d824ab537ee1450f30f1e400e161764bf7b
otherbanana <34509569+otherbanana@users.noreply.github.com>
2020-03-30 15:30:01 +0800

    Delete quanx.js

--- Files/quanx.js
+++
@@ -1,6 +0,0 @@
-var body = $response.body;
-body = '\/*\n@supported E4CF989D7F77\n*\/\n' + body;
-
-$done(body);
-//远程引用大佬Js订阅自动加ID
-//^https:\/\/(raw.githubusercontent|\w+\.github)\.(com|io)\/.*\.js$ url script-response-body quanx.js
