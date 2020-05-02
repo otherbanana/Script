4bfc653bd7ff0503b030d9e2d0737569ca2479e1
otherbanana <34509569+otherbanana@users.noreply.github.com>
2020-05-02 22:11:47 +0800

    Add files via upload

---
+++ Mitmproxy/delete header.py
@@ -0,0 +1,14 @@
+from mitmproxy import ctx, http
+import json
+import re
+
+class RemoveHeaders:
+	def response(self, flow):
+
+
+####### Test Remo #######
+		if flow.request.url.startswith("https://api.faceapp.io/api/v3.3/auth/user/credentials") \
+		and ('x-faceapp-errorcode') in flow.response.headers:
+			ctx.log.warn("Match Header= [{}]".format(flow.response.headers["x-faceapp-errorcode"]))
+			del flow.response.headers['x-faceapp-errorcode']
+addons=[RemoveHeaders()]
