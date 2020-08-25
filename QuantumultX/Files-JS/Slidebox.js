a4b203a28a1e7e01613c50187a4e0ffd5ab1e93b
bananatest <1993385765@qq.com>
2020-08-25 14:54:40 +0800

    updata

--- QuantumultX/Files-JS/Slidebox.js
+++ QuantumultX/Files-JS/Slidebox.js
@@ -1,6 +1,5 @@
 let obj= JSON.parse($response.body);
-obj= {
-  "expireTimestampMs": "1756543902000",
-  "isExpired": "0",
+obj['expireTimestampMs'] = '1756543902000'
+obj['isExpired'] = '0'
 };
 $done({body: JSON.stringify(obj)});
