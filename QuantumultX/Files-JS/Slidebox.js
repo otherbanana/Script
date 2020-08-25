25197c3d18a151a8df1ef6059ebb9f34a39480ae
otherbanana <34509569+otherbanana@users.noreply.github.com>
2020-08-25 14:56:15 +0800

    Update Slidebox.js

--- QuantumultX/Files-JS/Slidebox.js
+++ QuantumultX/Files-JS/Slidebox.js
@@ -1,5 +1,4 @@
 let obj= JSON.parse($response.body);
-obj['expireTimestampMs'] = '1756543902000'
+obj['expireTimestampMs'] = '1756543902000',
 obj['isExpired'] = '0'
-};
 $done({body: JSON.stringify(obj)});
