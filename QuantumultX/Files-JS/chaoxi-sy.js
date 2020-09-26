47716e78160e176a0743d1f1ea334666677313ea
otherbanana <34509569+otherbanana@users.noreply.github.com>
2020-09-26 23:31:58 +0800

    Update chaoxi-sy.js

--- QuantumultX/Files-JS/chaoxi-sy.js
+++ QuantumultX/Files-JS/chaoxi-sy.js
@@ -2,6 +2,6 @@ var modifiedHeaders = $request.headers;
 
 modifiedHeaders['Cookies'] = 'tide_sid=9486b41b6f814d2556b2e3b55a9a8738';
 
-var modifiedPath = '/v1/users/self/scenes/*/resources_urls';
+var modifiedPath = '/api2/abc?k=v';
 
 $done({path: modifiedPath, headers : modifiedHeaders});
