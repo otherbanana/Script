6eb789e2de9330c8de9b7a61701c682eab5dd958
otherbanana <34509569+otherbanana@users.noreply.github.com>
2020-09-26 15:18:05 +0800

    Update chaoxi-vip.js

--- QuantumultX/Files-JS/chaoxi-vip.js
+++ QuantumultX/Files-JS/chaoxi-vip.js
@@ -1,5 +1,4 @@
-var body = $response.body;
-var obj = JSON.parse(body);
+let obj=JSON.parse($response.body)
 
 obj["vip"]["expired_at"] = 1903393240
 obj["vip"]["created_at"] = 1590549571
@@ -9,4 +8,4 @@ obj["premium_type"] = 3
 obj["is_paying_premium_user"] = True
 body = JSON.stringify(obj);
 
-$done(body);
+$done({body:JSON.stringify(obj)})
